0c2120d72d3cabecbea3114536f2bc1d3babc7a6
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2023-10-09 23:15:29 -0500

    change(checkService): better split codebase

--- workers/f-check-service.js
+++ workers/f-check-service.js
@@ -1,84 +1,64 @@
 'use strict'
 
+import { Job } from 'bullmq'
+import { ApiPromise, WsProvider } from '@polkadot/api'
+
 import cfg from '../config/index.js'
-import { Logger } from '../lib/utils.js'
+
 import { ProviderServiceEntity } from '../domain/provider-service.entity.js'
-import { HealthChecker } from '../lib/health-checker.js'
-import { TimeoutException } from '../lib/check-service.js'
+
+import { HealthChecker, retry, asyncCallWithTimeout } from '../lib/check-service/index.js'
+import { Logger } from '../lib/utils.js'
 
 const logger = new Logger('worker:checkService')
 
-async function retry(fn, job, retriesLeft = 3, interval = 1000) {
-  try {
-    return await fn()
-  } catch (error) {
-    // typically a timeout error
-    logger.log('attempt', retriesLeft)
-    logger.error(error, error.stack)
-    job.log('attempt', retriesLeft)
-    job.log(error)
-    if (retriesLeft) {
-      // Wait interval milliseconds before next try
-      await new Promise((resolve) => setTimeout(resolve, interval))
-      return retry(fn, job, retriesLeft - 1, interval)
-    } else {
-      throw new Error('Max retries exceeded')
-    }
-  }
-}
-
 /**
  *
+ * @param {Job} job
  * @param {Object} params
- * @param {NodeJS.Timeout?} params.timeout
  * @param {HealthChecker} params.healthChecker
  * @param {ProviderServiceEntity} params.providerService
  */
-async function performCheck(job, params) {
-  const { healthChecker, providerService } = params
+async function performCheck(job, { healthChecker, providerService }) {
   job.log(`checkService: ${providerService}`)
 
+  /** @type {WsProvider?} */ let provider
+  /** @type {ApiPromise?} */ let api
   try {
-    logger.log('connecting to provider...')
+    provider = await healthChecker.buildProvider()
+
     job.log('connecting to provider...')
-    const provider = await healthChecker.connectProvider()
-    logger.log('provider is ready...')
-    job.log('provider is ready...')
+    await asyncCallWithTimeout(healthChecker.connectProvider(provider), 15 * 1_000)
+    job.log('provider is ready!')
 
-    const api = await healthChecker.buildApi(provider)
+    job.updateProgress(25)
 
-    /** @type {NodeJS.timeoutHandle} */ let timeoutHandle
-    const timeoutPromise = new Promise((_, reject) => {
-      timeoutHandle = setTimeout(async () => {
-        await api.disconnect()
-        await provider.disconnect()
-        await healthChecker.connectionStrategy.after()
-        await job.discard()
+    api = await healthChecker.buildApi(provider)
 
-        reject(new TimeoutException())
-      }, 70 * 1000)
-    })
-
-    logger.log('connecting to api...')
     job.log('connecting to api...')
-    await Promise.race([healthChecker.connectApi(api), timeoutPromise]).then(() => {
-      clearTimeout(timeoutHandle)
-    })
-    logger.log('api is ready...')
+    await asyncCallWithTimeout(healthChecker.connectApi(api), 15 * 1_000)
     job.log('api is ready...')
+
+    job.updateProgress(60)
 
     logger.log('getting stats from provider / api...')
     job.log('getting stats from provider / api...')
     const result = await healthChecker.tryCheck(api, cfg.performance?.sla)
 
-    await api.disconnect()
-    await provider.disconnect()
+    job.updateProgress(100)
+
+    await api?.disconnect()
+    await provider?.disconnect()
     await healthChecker.connectionStrategy.after()
 
     return result
   } catch (err) {
-    logger.error(err, err.stack)
-    job.log('== got error for ', healthChecker.connectionStrategy.getEndpoint())
+    await api?.disconnect()
+    await provider?.disconnect()
+    await healthChecker.connectionStrategy.after()
+
+    logger.error(err)
+    job.log('>> got error for ', healthChecker.connectionStrategy.getEndpoint())
     job.log(err.toString())
 
     throw err
@@ -87,16 +67,21 @@ async function performCheck(job, params) {
 
 /**
  * Similar to healthCheck-endpoint, but for IBP url at member.services_address
- * @param {} job
+ * @param {Job} job
  * @returns
  */
 export async function checkService(job) {
   /** @type {ProviderServiceEntity} */
   const providerService = job.data.providerService
+  const {
+    service: { id: serviceId },
+    provider: { id: providerId },
+  } = providerService
+
   /** @type {string} */
   const monitorId = job.data.monitorId
 
-  logger.debug('Start checking', providerService)
+  logger.debug(`Checking ${serviceId} for ${providerId}`)
   const healthChecker = new HealthChecker({
     monitorId,
     providerService,
@@ -114,19 +99,19 @@ export async function checkService(job) {
       5 * 1000 // wait 5 seconds between each try
     )
 
-    logger.log('Done', providerService)
-    job.log('[checkService] Done', providerService)
+    logger.log(`Done checking ${serviceId} for ${providerId}`)
+    job.log(`[checkService] Done checking ${serviceId} for ${providerId}`)
 
     return result
   } catch (err) {
-    logger.warn('WE GOT AN ERROR AFTER RETRIES --------------')
+    logger.warn('WE GOT AN ERROR AFTER RETRIES')
     logger.error(err)
     job.log('WE GOT AN ERROR AFTER RETRIES --------------')
     job.log(err)
     job.log(err.toString())
 
-    logger.log('Done with error', providerService)
-    job.log('[checkService] Done with error', providerService)
+    logger.log(`Done checking ${serviceId} for ${providerId} with error`)
+    job.log(`[checkService] Done checking ${serviceId} for ${providerId} with error`)
 
     return healthChecker.errorCheck()
   }
